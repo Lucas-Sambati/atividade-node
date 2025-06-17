9d16c9fd92bf09801bac84548d6d4fc6ea2beaec
Lucas-Sambati <lucasleonardosambati@hotmail.com>
2025-06-17 20:42:20 -0300

    Correção do Professor

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, world!\n');
+app.get('/', (req, res) => {
+res.send('Hello World from Node.js!');
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+app.listen(port, () => {
+console.log(`Servidor rodando na porta ${port}`);
 });
